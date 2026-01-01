3b87ef56e3c9ae64d5a1ced160e1a9c052a3aa0d
GrayJack <gr41.j4ck@gmail.com>
2026-01-01 17:16:29 -0300

    feat: Add `pspvideocoded.h`

---
+++ src/pspvideocodec.h
@@ -0,0 +1,39 @@
+/*
+ * PSP Software Development Kit - https://github.com/pspdev
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * pspvideocodec.h - Prototypes for the sceVideocodec library.
+ *
+ * Copyright (c) 2007 cooleyes
+ *
+ */
+
+#ifndef __SCELIBVIDEOCODEC_H__
+#define __SCELIBVIDEOCODEC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#ifdef __USER__
+
+int sceVideocodecOpen(unsigned long *Buffer, int Type);
+int sceVideocodecGetEDRAM(unsigned long *Buffer, int Type);
+int sceVideocodecInit(unsigned long *Buffer, int Type);
+int sceVideocodecDecode(unsigned long *Buffer, int Type);
+int sceVideocodecReleaseEDRAM(unsigned long *Buffer);
+
+// sceVideocodecGetVersion
+// sceVideocodecScanHeader
+// sceVideocodecDelete
+// sceVideocodecSetMemory
+// sceVideocodecStop
+
+#endif // __USER__
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __SCELIBVIDEOCODEC_H__
