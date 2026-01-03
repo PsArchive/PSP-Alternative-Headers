392cb390ac26f8c65c3aa021215df66cde9332db
GrayJack <gr41.j4ck@gmail.com>
2026-01-03 14:42:51 -0300

    feat: Add `pspdebug.h ` for the `KDebugForKernel` modules

---
+++ src/pspkdebug.h
@@ -0,0 +1,62 @@
+/*
+ * PSP Software Development Kit - https://github.com/pspdev
+ * -----------------------------------------------------------------------
+ * Licensed under the BSD license, see LICENSE in PSPSDK root for details.
+ *
+ * pspkdebug.h - Interface to KDebugForKernel.
+ *
+ * Copyright (c) 2005 James Forshaw <tyranid@gmail.com>
+ *
+ */
+
+#ifndef PSPKDEBUG_H
+#define PSPKDEBUG_H
+
+#include <pspkerneltypes.h>
+
+/** @defgroup Kdebug Interface to the KDebugForKernel library.
+ */
+
+/** @addtogroup Kdebug Interface to the KDebugForKernel library. */
+/**@{*/
+
+/** Typedef for the debug putcharacter handler */
+typedef void (*PspDebugPutChar)(unsigned short* args, unsigned int ch);
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef __KERNEL__
+
+/**
+ * Register a debug put character handler
+ *
+ * @param func - The put character function to register.
+ */
+void sceKernelRegisterDebugPutchar(PspDebugPutChar func);
+
+/**
+ * Get the debug put character handler
+ *
+ * @return The current debug putchar handler
+ */
+PspDebugPutChar sceKernelGetDebugPutchar(void);
+
+/**
+ * Kernel printf function.
+ *
+ * @param format - The format string.
+ * @param ... - Arguments for the format string.
+ */
+void Kprintf(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+#endif /* __KERNEL__ */
+
+/**@}*/
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* PSPKDEBUG_H */
